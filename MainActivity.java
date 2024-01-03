eec777e030de88a9b051cc7975f587ddd7d18659
Priyasenthil17 <130549990+Priyasenthil17@users.noreply.github.com>
2024-01-03 15:50:58 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+   {
+        
+// write coding part for onCreate()
+    }
+    protected void onStart()
+    {
+       // write coding part for onStart()
+    }
+    protected void onResume()
+   {
+       // write coding part for onResume()
+
+    }
+    protected void onPause()
+   {
+         // write coding part for onPause()
+
+    }
+    protected void onStop()
+   {
+        // write coding part for onStop()
+    }
+    protected void onRestart() 
+   {
+         // write coding part for onRestart()
+
+    }
+    protected void onDestroy()
+  {
+         // write coding part for onDestroy()
+
+    }
+}
+
